7cdfb46e624f4855cc5714f5c3a39f9c5f3a79a8
Noah Sherwin <noahrsherwin@gmail.com>
2022-10-14 22:53:09 -0700

    feat: add class FileLockerEx for file handle locks

---
+++ deadlock-dotnet-sdk/Domain/FileLockerEx.cs
@@ -0,0 +1,46 @@
+﻿namespace deadlock_dotnet_sdk.Domain
+{
+    public class FileLockerEx
+    {
+        #region Properties
+
+        /// <summary>
+        /// Get or set the path of the file that is locked
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Get or set the List of Process objects that are locking the file
+        /// </summary>
+        public List<SafeHandleEx> Lockers { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Initialize a new FileLocker
+        /// </summary>
+        public FileLockerEx()
+        {
+            Path = "";
+            Lockers = new List<SafeHandleEx>();
+        }
+
+        /// <summary>
+        /// Initialize a new FileLocker
+        /// </summary>
+        /// <param name="path">The path of the file</param>
+        /// <param name="lockers">The List of Process objects that are locking the file</param>
+        public FileLockerEx(string path, List<SafeHandleEx> lockers)
+        {
+            Path = path;
+            Lockers = lockers;
+        }
+
+        public static FileLockerEx GetFileLockerEx(string path, NativeMethods.Filter filter)
+        {
+            return new(
+                path,
+                NativeMethods.FindLockingHandles(path, filter)
+                );
+        }
+    }
+}
