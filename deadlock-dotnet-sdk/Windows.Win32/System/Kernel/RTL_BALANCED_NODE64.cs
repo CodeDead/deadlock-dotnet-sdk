72c5d2dfd39e3da114706751698330e72293f7f5
Noah Sherwin <noahrsherwin@gmail.com>
2023-03-27 04:26:53 -0700

    fix: prevent runtime TypeLoadException
    Though it Roslyn didn't catch it, this appears to be CS0523:
    > Struct member 'ExampleType.ExampleMember' of type 'ExampleType' causes a cycle in the struct layout
    
    ExampleType cannot have a member of type ExampleType.
    Likewise, ExampleType<T> cannot have a member of type ExampleType<T>

--- deadlock-dotnet-sdk/Windows.Win32/System/Kernel/RTL_BALANCED_NODE64.cs
+++ deadlock-dotnet-sdk/Windows.Win32/System/Kernel/RTL_BALANCED_NODE64.cs
@@ -11,12 +11,15 @@ internal unsafe struct RTL_BALANCED_NODE64
 {
     [FieldOffset(0x00)] internal fixed uint _Children[2];
     internal UIntPtr64<RTL_BALANCED_NODE64>[] Children => new UIntPtr64<RTL_BALANCED_NODE64>[] { _Children[0], _Children[1] };
-    [FieldOffset(0x00)] internal UIntPtr64<RTL_BALANCED_NODE64> Left;
-    [FieldOffset(0x08)] internal UIntPtr64<RTL_BALANCED_NODE64> Right;
+    [FieldOffset(0x00)] private UIntPtr64/* <RTL_BALANCED_NODE64> */ left;
+    [FieldOffset(0x08)] private UIntPtr64/* <RTL_BALANCED_NODE64> */ right;
 
     [FieldOffset(0x10)] internal UIntPtr64 ParentValue;
     /// <summary>applies if the node is in a Red Black tree</summary>
     internal byte Red => (byte)(ParentValue & 0b1);
     /// <summary>applies if the node is in an AVL tree</summary>
     internal byte Balance => (byte)((ParentValue >> 1) & 0b11);
+
+    internal UIntPtr64<RTL_BALANCED_NODE64> Left { get => (UIntPtr64<RTL_BALANCED_NODE64>)left; set => left = (UIntPtr64)value; }
+    internal UIntPtr64<RTL_BALANCED_NODE64> Right { get => (UIntPtr64<RTL_BALANCED_NODE64>)right; set => right = (UIntPtr64)value; }
 }
