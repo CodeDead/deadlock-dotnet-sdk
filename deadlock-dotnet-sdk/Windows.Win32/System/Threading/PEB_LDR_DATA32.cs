cf0998478306ea469e54413ae5e71178a627db06
Noah Sherwin <noahrsherwin@gmail.com>
2023-03-27 04:16:51 -0700

    fix: use non-generic LIST_ENTRY LIST_ENTRY instances can form a list of objects of many different Types. Specifying a Type using LIST_ENTRY<T> is too restrictive.

--- deadlock-dotnet-sdk/Windows.Win32/System/Threading/PEB_LDR_DATA32.cs
+++ deadlock-dotnet-sdk/Windows.Win32/System/Threading/PEB_LDR_DATA32.cs
@@ -14,10 +14,12 @@ internal readonly struct PEB_LDR_DATA32
     [FieldOffset(0x00)] internal readonly uint Length;
     [FieldOffset(0x04)] internal readonly BOOLEAN Initialized;
     [FieldOffset(0x08)] internal readonly UIntPtr32 SsHandle;
-    [FieldOffset(0x0C)] internal readonly LIST_ENTRY32<LDR_DATA_TABLE_ENTRY32> InLoadOrderModuleList;
-    /// <summary>The head of a doubly-linked list that contains the loaded modules for the process. Each item in the list is a pointer to an <b>LDR_DATA_TABLE_ENTRY</b> structure. For more information, see Remarks.</summary>
-    [FieldOffset(0x14)] internal readonly LIST_ENTRY32<LDR_DATA_TABLE_ENTRY32> InMemoryOrderModuleList;
-    [FieldOffset(0x1C)] internal readonly LIST_ENTRY32<LDR_DATA_TABLE_ENTRY32> InInitializationOrderModuleList;
+    /// <summary>The head of a doubly-linked list that contains the loaded modules for the process in the order they were loaded. Each item in the list is a pointer to an <see cref="LDR_DATA_TABLE_ENTRY32"/> structure. See <see href="https://learn.microsoft.com/en-us/windows-hardware/drivers/kernel/singly-and-doubly-linked-lists#doubly-linked-lists">Double Linked Lists</see></summary>
+    [FieldOffset(0x0C)] internal readonly LIST_ENTRY32 InLoadOrderModuleList;
+    /// <summary>The head of a doubly-linked list that contains the loaded modules for the process in the order they appear in memory. Each item in the list is a pointer to an <see cref="LDR_DATA_TABLE_ENTRY64"/> structure. See <see href="https://learn.microsoft.com/en-us/windows-hardware/drivers/kernel/singly-and-doubly-linked-lists#doubly-linked-lists">Double Linked Lists</see></summary>
+    [FieldOffset(0x14)] internal readonly LIST_ENTRY32 InMemoryOrderModuleList;
+    /// <summary>The head of a doubly-linked list that contains the loaded modules for the process in the order they were initialized. Each item in the list is a pointer to an <see cref="LDR_DATA_TABLE_ENTRY64"/> structure. See <see href="https://learn.microsoft.com/en-us/windows-hardware/drivers/kernel/singly-and-doubly-linked-lists#doubly-linked-lists">Double Linked Lists</see></summary>
+    [FieldOffset(0x1C)] internal readonly LIST_ENTRY32 InInitializationOrderModuleList;
     /// <summary>5.1 and higher</summary>
     [FieldOffset(0x24)] internal readonly UIntPtr32 EntryInProgress;
     /// <summary>late 6.0 and higher</summary>
--- deadlock-dotnet-sdk/Windows.Win32/System/Threading/PEB_LDR_DATA64.cs
+++ deadlock-dotnet-sdk/Windows.Win32/System/Threading/PEB_LDR_DATA64.cs
@@ -13,10 +13,12 @@ internal readonly struct PEB_LDR_DATA64
     [FieldOffset(0x00)] internal readonly uint Length;
     [FieldOffset(0x04)] internal readonly BOOLEAN Initialized;
     [FieldOffset(0x08)] internal readonly unsafe UIntPtr64 SsHandle;
-    [FieldOffset(0x10)] internal readonly LIST_ENTRY64<LDR_DATA_TABLE_ENTRY64> InLoadOrderModuleList;
-    /// <summary>The head of a doubly-linked list that contains the loaded modules for the process. Each item in the list is a pointer to an <b>LDR_DATA_TABLE_ENTRY</b> structure. For more information, see Remarks.</summary>
-    [FieldOffset(0x20)] internal readonly LIST_ENTRY64<LDR_DATA_TABLE_ENTRY64> InMemoryOrderModuleList;
-    [FieldOffset(0x30)] internal readonly LIST_ENTRY64<LDR_DATA_TABLE_ENTRY64> InInitializationOrderModuleList;
+    /// <summary>The head of a doubly-linked list that contains the loaded modules for the process in the order they were loaded. Each item in the list is a pointer to an <see cref="LDR_DATA_TABLE_ENTRY64"/> structure. See <see href="https://learn.microsoft.com/en-us/windows-hardware/drivers/kernel/singly-and-doubly-linked-lists#doubly-linked-lists">Double Linked Lists</see></summary>
+    [FieldOffset(0x10)] internal readonly LIST_ENTRY64 InLoadOrderModuleList;
+    /// <summary>The head of a doubly-linked list that contains the loaded modules for the process in the order they appear in memory. Each item in the list is a pointer to an <see cref="LDR_DATA_TABLE_ENTRY64"/> structure. See <see href="https://learn.microsoft.com/en-us/windows-hardware/drivers/kernel/singly-and-doubly-linked-lists#doubly-linked-lists">Double Linked Lists</see></summary>
+    [FieldOffset(0x20)] internal readonly LIST_ENTRY64 InMemoryOrderModuleList;
+    /// <summary>The head of a doubly-linked list that contains the loaded modules for the process in the order they were initialized. Each item in the list is a pointer to an <see cref="LDR_DATA_TABLE_ENTRY64"/> structure. See <see href="https://learn.microsoft.com/en-us/windows-hardware/drivers/kernel/singly-and-doubly-linked-lists#doubly-linked-lists">Double Linked Lists</see></summary>
+    [FieldOffset(0x30)] internal readonly LIST_ENTRY64 InInitializationOrderModuleList;
     [FieldOffset(0x40)] internal readonly unsafe UIntPtr64 EntryInProgress; // 5.1 and higher
     [FieldOffset(0x48)] internal readonly BOOLEAN ShutdownInProgress; // late 6.0 and higher
     [FieldOffset(0x50)] internal readonly HANDLE64 ShutdownThreadId; // late 6.0 and higher
