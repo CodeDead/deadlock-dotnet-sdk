19469b6f8f199252b22ae41b083976abc6cc6127
Noah Sherwin <noahrsherwin@gmail.com>
2023-03-27 03:55:28 -0700

    refactor: move UIntPtr64<T> to new file

--- deadlock-dotnet-sdk/Windows.Win32/UIntPtr64.cs
+++ deadlock-dotnet-sdk/Windows.Win32/UIntPtr64.cs
@@ -10,13 +10,3 @@ internal struct UIntPtr64
     public static implicit operator UIntPtr64(ulong v) => new() { Value = v };
     public static implicit operator ulong(UIntPtr64 v) => v.Value;
 }
-
-internal struct UIntPtr64<T> where T : unmanaged
-{
-    public ulong Value;
-
-    public static implicit operator UIntPtr64<T>(ulong v) => new() { Value = v };
-    public static implicit operator ulong(UIntPtr64<T> v) => v.Value;
-
-    public static explicit operator UIntPtr64(UIntPtr64<T> v) => v.Value;
-}
---
+++ deadlock-dotnet-sdk/Windows.Win32/UIntPtr64_T.cs
@@ -0,0 +1,13 @@
+namespace Windows.Win32;
+
+/// <inheritdoc cref="UIntPtr64"/>
+internal struct UIntPtr64<T> where T : unmanaged
+{
+    public ulong Value;
+
+    public static implicit operator UIntPtr64<T>(ulong v) => new() { Value = v };
+    public static implicit operator ulong(UIntPtr64<T> v) => v.Value;
+
+    public static explicit operator UIntPtr64(UIntPtr64<T> v) => v.Value;
+    public static explicit operator UIntPtr64<T>(UIntPtr64 v) => new() { Value = v.Value };
+}
