3eca4145466540cb17cb5176bcb669fdb36a73a2
Noah Sherwin <noahrsherwin@gmail.com>
2023-03-27 04:10:31 -0700

    chore: disable CS0649 for readonly interop structs

--- deadlock-dotnet-sdk/Windows.Win32/System/Threading/RTL_CRITICAL_SECTION32.cs
+++ deadlock-dotnet-sdk/Windows.Win32/System/Threading/RTL_CRITICAL_SECTION32.cs
@@ -2,11 +2,13 @@ namespace Windows.Win32.System.Threading
 {
     internal readonly struct RTL_CRITICAL_SECTION32
     {
+#pragma warning disable CS0649
         internal readonly unsafe UIntPtr32<RTL_CRITICAL_SECTION_DEBUG> DebugInfo;
         internal readonly int LockCount;
         internal readonly int RecursionCount;
         internal readonly Foundation.HANDLE32 OwningThread;
         internal readonly Foundation.HANDLE32 LockSemaphore;
         internal readonly uint SpinCount;
+#pragma warning restore CS0649
     }
 }
--- deadlock-dotnet-sdk/Windows.Win32/System/Threading/RTL_CRITICAL_SECTION64.cs
+++ deadlock-dotnet-sdk/Windows.Win32/System/Threading/RTL_CRITICAL_SECTION64.cs
@@ -2,11 +2,13 @@ namespace Windows.Win32.System.Threading
 {
     internal readonly struct RTL_CRITICAL_SECTION64
     {
+#pragma warning disable CS0649
         internal readonly unsafe UIntPtr64<RTL_CRITICAL_SECTION_DEBUG> DebugInfo;
         internal readonly int LockCount;
         internal readonly int RecursionCount;
         internal readonly Foundation.HANDLE64 OwningThread;
         internal readonly Foundation.HANDLE64 LockSemaphore;
         internal readonly ulong SpinCount;
+#pragma warning restore CS0649
     }
 }
