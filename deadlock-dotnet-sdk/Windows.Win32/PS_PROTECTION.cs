48d4d12334d0fce13755b1cef1f45cf8fcd9b708
Noah Sherwin <noahrsherwin@gmail.com>
2023-04-01 19:40:29 -0700

    refactor: change PS_PROTECTION to public, add enums

--- deadlock-dotnet-sdk/Windows.Win32/PS_PROTECTION.cs
+++ deadlock-dotnet-sdk/Windows.Win32/PS_PROTECTION.cs
@@ -2,10 +2,32 @@ namespace Windows.Win32;
 /// <summary>
 /// https://learn.microsoft.com/en-us/windows/win32/procthread/zwqueryinformationprocess#PS_PROTECTION
 /// </summary>
-internal struct PS_PROTECTION
+public struct PS_PROTECTION
 {
     public byte Level;
-    public byte Type => (byte)((Level >> 0) & 0b111);
-    public byte Audit => (byte)((Level >> 3) & 0b1); // Reserved
-    public byte Signer => (byte)((Level >> 4) & 0b1111);
+    public PS_PROTECTED_TYPE Type => (PS_PROTECTED_TYPE)((Level >> 0) & 0b111);
+    /// <summary>Reserved</summary>
+    public byte Audit => (byte)((Level >> 3) & 0b1);
+    public PS_PROTECTED_SIGNER Signer => (PS_PROTECTED_SIGNER)((Level >> 4) & 0b1111);
+
+    public enum PS_PROTECTED_TYPE
+    {
+        PsProtectedTypeNone = 0,
+        PsProtectedTypeProtectedLight = 1,
+        PsProtectedTypeProtected = 2
+    }
+
+    public enum PS_PROTECTED_SIGNER
+    {
+        PsProtectedSignerNone = 0,
+        PsProtectedSignerAuthenticode,
+        PsProtectedSignerCodeGen,
+        PsProtectedSignerAntimalware,
+        PsProtectedSignerLsa,
+        PsProtectedSignerWindows,
+        PsProtectedSignerWinTcb,
+        PsProtectedSignerWinSystem,
+        PsProtectedSignerApp,
+        PsProtectedSignerMax
+    }
 }
