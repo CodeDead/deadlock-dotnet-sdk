a07d7b500c950f383baba5ae7a2a7ab2280f887a
Noah Sherwin <noahrsherwin@gmail.com>
2023-04-03 21:32:40 -0700

    refactor: implement IEquatable for PS_PROTECTION

--- deadlock-dotnet-sdk/Windows.Win32/PS_PROTECTION.cs
+++ deadlock-dotnet-sdk/Windows.Win32/PS_PROTECTION.cs
@@ -2,13 +2,15 @@ namespace Windows.Win32;
 /// <summary>
 /// https://learn.microsoft.com/en-us/windows/win32/procthread/zwqueryinformationprocess#PS_PROTECTION
 /// </summary>
-public struct PS_PROTECTION
+public struct PS_PROTECTION : IEquatable<PS_PROTECTION>
 {
     public byte Level;
     public PS_PROTECTED_TYPE Type => (PS_PROTECTED_TYPE)((Level >> 0) & 0b111);
     /// <summary>Reserved</summary>
     public byte Audit => (byte)((Level >> 3) & 0b1);
     public PS_PROTECTED_SIGNER Signer => (PS_PROTECTED_SIGNER)((Level >> 4) & 0b1111);
+
+    public bool Equals(PS_PROTECTION other) => Level == other.Level;
 
     public enum PS_PROTECTED_TYPE
     {
@@ -30,4 +32,10 @@ public struct PS_PROTECTION
         PsProtectedSignerApp,
         PsProtectedSignerMax
     }
+
+    public override bool Equals(object? obj) => obj is PS_PROTECTION pS_PROTECTION && Equals(pS_PROTECTION);
+
+    public static bool operator ==(PS_PROTECTION left, PS_PROTECTION right) => left.Equals(right);
+
+    public static bool operator !=(PS_PROTECTION left, PS_PROTECTION right) => !(left == right);
 }
