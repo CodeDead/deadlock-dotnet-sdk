7831d99d5258f65601ee3fc6421637cfca8a01e3
Noah Sherwin <noahrsherwin@gmail.com>
2023-04-03 21:28:01 -0700

    revert: remove try-catch from GetHandleObjectType
    Partially reverts 894e6792 "refactor: replace PhGetObjectTypeName with custom solution"

--- deadlock-dotnet-sdk/Windows.Win32/SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX.cs
+++ deadlock-dotnet-sdk/Windows.Win32/SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX.cs
@@ -55,35 +55,26 @@ public readonly struct SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX
 #pragma warning restore RCS1213, CS0649, CS0169, IDE0051
 
     /// <summary>Get the Type of the object as a string</summary>
-    /// <exception cref="Exception">P/Invoke function NtQueryObject failed. See Exception data.</exception>
     /// <returns>The Type of the object as a string.</returns>
     public unsafe string GetHandleObjectType()
     {
-        try
-        {
-            NTSTATUS status;
-            using SafeBuffer<PUBLIC_OBJECT_TYPE_INFORMATION> buffer = new(numBytes: 256/* (nuint)Marshal.SizeOf<PUBLIC_OBJECT_TYPE_INFORMATION>() */);
-            uint returnLength;
-            using var h = new SafeObjectHandle(HandleValue, false);
+        NTSTATUS status;
+        using SafeBuffer<PUBLIC_OBJECT_TYPE_INFORMATION> buffer = new(numBytes: 256/* (nuint)Marshal.SizeOf<PUBLIC_OBJECT_TYPE_INFORMATION>() */);
+        uint returnLength;
+        using var h = new SafeObjectHandle(HandleValue, false);
 
+        status = NtQueryObject(h, OBJECT_INFORMATION_CLASS.ObjectTypeInformation, (void*)buffer.DangerousGetHandle(), (uint)buffer.ByteLength, &returnLength);
+
+        // Something's off. Marshal.SizeOf() returns 0x68 (104) but returnLength is 0x78 (120) or sometimes 0x80 (128). Is Win32Metadata's type definition wrong?
+        while (status.Code is STATUS_BUFFER_OVERFLOW or STATUS_INFO_LENGTH_MISMATCH or STATUS_BUFFER_TOO_SMALL)
+        {
+            buffer.Reallocate(returnLength);
             status = NtQueryObject(h, OBJECT_INFORMATION_CLASS.ObjectTypeInformation, (void*)buffer.DangerousGetHandle(), (uint)buffer.ByteLength, &returnLength);
-
-            // Something's off. Marshal.SizeOf() returns 0x68 (104) but returnLength is 0x78 (120) or sometimes 0x80 (128). Is Win32Metadata's type definition wrong?
-            while (status.Code is STATUS_BUFFER_OVERFLOW or STATUS_INFO_LENGTH_MISMATCH or STATUS_BUFFER_TOO_SMALL)
-            {
-                buffer.Reallocate(returnLength);
-                status = NtQueryObject(h, OBJECT_INFORMATION_CLASS.ObjectTypeInformation, (void*)buffer.DangerousGetHandle(), (uint)buffer.ByteLength, &returnLength);
-            }
-
-            if (status == STATUS_INVALID_HANDLE || !status.IsSuccessful)
-                throw new NTStatusException(status);
-
-            return (string)buffer.Read<PUBLIC_OBJECT_TYPE_INFORMATION>(0).TypeName;
         }
-        catch (Exception)
-        {
-            throw;
-        }
+
+        return status.IsSuccessful
+            ? (string)buffer.Read<PUBLIC_OBJECT_TYPE_INFORMATION>(0).TypeName
+            : throw new NTStatusException(status);
     }
 
     internal unsafe HANDLE_FLAGS GetHandleInfo()
