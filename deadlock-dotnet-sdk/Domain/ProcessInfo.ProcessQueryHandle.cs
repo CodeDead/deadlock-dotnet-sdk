2f461e0cb129dabc602cdea6d64423f9426b4837
Noah Sherwin <noahrsherwin@gmail.com>
2023-04-16 18:05:16 -0700

    feat: add class ProcessQueryHandle to store access rights with handle refactor: remove per-ProcessInfo handle lists feat: add ProcessId property to access underlying Process Id

---
+++ deadlock-dotnet-sdk/Domain/ProcessInfo.ProcessQueryHandle.cs
@@ -0,0 +1,43 @@
+using Microsoft.Win32.SafeHandles;
+using Windows.Win32.System.Threading;
+using static Windows.Win32.PInvoke;
+using Win32Exception = System.ComponentModel.Win32Exception;
+
+namespace deadlock_dotnet_sdk.Domain;
+
+public partial class ProcessInfo
+{
+    public class ProcessQueryHandle
+    {
+        public ProcessQueryHandle(SafeProcessHandle processHandle, PROCESS_ACCESS_RIGHTS accessRights)
+        {
+            Handle = processHandle;
+            AccessRights = accessRights;
+        }
+
+        public SafeProcessHandle Handle { get; }
+        public PROCESS_ACCESS_RIGHTS AccessRights { get; }
+
+        /// <summary>
+        /// Open a handle with the requested rights for a process.
+        /// </summary>
+        /// <param name="processId"></param>
+        /// <param name="accessRights"></param>
+        /// <returns>A ProcessQueryHandle wrapping a SafeProcessHandle and the requested access rights.</returns>
+        /// <exception cref="Win32Exception">Failed to open handle. The process might not exist, access was denied, or an unknown error occurred.<br/>
+        ///     <example>
+        ///     If processId is 0, error code is ERROR_INVALID_PARAMETER<br/>
+        ///     If process is System (4), CRSS, or similarly protected processes, error code is ERROR_ACCESS_DENIED<br/>
+        ///     </example>
+        /// </exception>
+        /// <remarks>
+        /// - If processId == Process.GetCurrentProcess().Id, use Process.GetCurrentProcess().SafeHandle property instead.
+        /// - If Windows.Win32.PInvoke.IsDebugModeEnabled() == true, the requested access is granted regardless of the security descriptor. See GetSecurityInfo();
+        /// </remarks>
+        public static ProcessQueryHandle OpenProcessHandle(int processId, PROCESS_ACCESS_RIGHTS accessRights)
+        {
+            var h = OpenProcess_SafeHandle(accessRights, false, (uint)processId);
+            return h is null ? throw new Win32Exception() : (new(h, accessRights));
+        }
+    }
+}
--- deadlock-dotnet-sdk/Domain/ProcessInfo.cs
+++ deadlock-dotnet-sdk/Domain/ProcessInfo.cs
@@ -1,14 +1,90 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using PInvoke;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Threading;
+using Windows.Win32.System.WindowsProgramming;
+using static System.Environment;
+using static Windows.Win32.PInvoke;
+using static Windows.Win32.PS_PROTECTION.PS_PROTECTED_TYPE;
+using Code = PInvoke.NTSTATUS.Code;
+using NTSTATUS = Windows.Win32.Foundation.NTSTATUS;
+using Win32Exception = System.ComponentModel.Win32Exception;
 
 namespace deadlock_dotnet_sdk.Domain;
 
-public class ProcessInfo
+public partial class ProcessInfo
 {
+    private bool canGetQueryLimitedInfoHandle;
+    private bool canGetReadMemoryHandle;
+    private (ProcessQueryHandle? v, Exception? ex) processHandle;
+
     public ProcessInfo(Process process)
     {
         Process = process;
     }
 
+    /// <summary>The base Process object this instance expands upon.</summary>
     public Process Process { get; }
-    public List<SafeHandleEx> Handles { get; set; } = new();
+    public int ProcessId => Process.Id;
+
+    public (ProcessQueryHandle? v, Exception? ex) ProcessHandle
+    {
+        get
+        {
+            if (processHandle == default)
+            {
+                const string exMsg = "Unable to open handle; ";
+                // We can't lookup the ProcessProtection without opening a process handle to check the process protection.
+                //PROCESS_ACCESS_RIGHTS access = ProcessProtection.v?.Type is PS_PROTECTION.PS_PROTECTED_TYPE.PsProtectedTypeProtected ? PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION : PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_ACCESS_RIGHTS.PROCESS_VM_READ;
+
+                try
+                {
+                    return processHandle = (ProcessQueryHandle.OpenProcessHandle(
+                            ProcessId,
+                            PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_ACCESS_RIGHTS.PROCESS_VM_READ),
+                        null);
+                }
+                catch (Win32Exception ex) when ((Win32ErrorCode)ex.NativeErrorCode is Win32ErrorCode.ERROR_ACCESS_DENIED)
+                {
+                    // Before assuming anything, try without PROCESS_VM_READ. Without it, we don't need Debug privilege, but the PEB and all of its recursive members (e.g. Command Line) will be unavailable.
+                    const string exAccessMsg = exMsg + " The requested permissions were denied.";
+                    string exPermsFirst = NewLine + "First attempt's requested permissions: " + nameof(PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION) + ", " + nameof(PROCESS_ACCESS_RIGHTS.PROCESS_VM_READ);
+                    canGetReadMemoryHandle = false;
+
+                    try
+                    {
+                        return processHandle = (ProcessQueryHandle.OpenProcessHandle(ProcessId, PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION), null);
+                    }
+                    catch (Win32Exception ex2) when ((Win32ErrorCode)ex.NativeErrorCode is Win32ErrorCode.ERROR_ACCESS_DENIED)
+                    {
+                        // Debug Mode could not be enabled? Was SE_DEBUG_NAME denied to user or is current process not elevated?
+                        canGetQueryLimitedInfoHandle = false;
+                        string exPermsSecond = NewLine + "Second attempt's requested permissions: " + nameof(PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION);
+                        return (null, new UnauthorizedAccessException(exAccessMsg + exPermsFirst + exPermsSecond, ex2));
+                    }
+                    catch (Exception ex2)
+                    {
+                        canGetQueryLimitedInfoHandle = false;
+                        return (null, new AggregateException(exMsg + " Permissions were denied and an unknown error occurred.", new Exception[] { ex, ex2 }));
+                    }
+                }
+                catch (Win32Exception ex) when ((Win32ErrorCode)ex.NativeErrorCode is Win32ErrorCode.ERROR_INVALID_PARAMETER)
+                {
+                    return (null, new ArgumentException(exMsg + " A process with ID " + ProcessId + " could not be found. The process may have exited.", ex));
+                }
+                catch (Exception ex)
+                {
+                    // unknown error
+                    return (null, new Exception(exMsg + " An unknown error occurred.", ex));
+                }
+            }
+            else
+            {
+                return processHandle;
+            }
+        }
+    }
 }
