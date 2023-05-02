a1f886f68965fbe8b521ea129dbc93f737d021b1
Noah Sherwin <noahrsherwin@gmail.com>
2023-05-01 21:48:49 -0700

    refactor: add const error messages to ProcessHandle

--- deadlock-dotnet-sdk/Domain/ProcessInfo.cs
+++ deadlock-dotnet-sdk/Domain/ProcessInfo.cs
@@ -144,7 +144,7 @@ public partial class ProcessInfo
         {
             if (processHandle is (null, null))
             {
-                const string exMsg = "Unable to open handle; ";
+                const string errUnableMsg = "Unable to open handle; ";
                 // We can't lookup the ProcessProtection without opening a process handle to check the process protection.
                 //PROCESS_ACCESS_RIGHTS access = ProcessProtection.v?.Type is PS_PROTECTION.PS_PROTECTED_TYPE.PsProtectedTypeProtected ? PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION : PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_ACCESS_RIGHTS.PROCESS_VM_READ;
 
@@ -158,7 +158,7 @@ public partial class ProcessInfo
                 catch (Win32Exception ex) when (ex.NativeErrorCode is Win32ErrorCode.ERROR_ACCESS_DENIED)
                 {
                     // Before assuming anything, try without PROCESS_VM_READ. Without it, we don't need Debug privilege, but the PEB and all of its recursive members (e.g. Command Line) will be unavailable.
-                    const string exAccessMsg = exMsg + " The requested permissions were denied.";
+                    const string exAccessMsg = errUnableMsg + " The requested permissions were denied.";
                     const string exPermsFirst = "\r\nFirst attempt's requested permissions: " + nameof(PROCESS_ACCESS_RIGHTS.PROCESS_QUERY_LIMITED_INFORMATION) + ", " + nameof(PROCESS_ACCESS_RIGHTS.PROCESS_VM_READ);
 
                     try
@@ -175,17 +175,17 @@ public partial class ProcessInfo
                     }
                     catch (Exception ex2)
                     {
-                        return processHandle = (null, new AggregateException(exMsg + " Permissions were denied and an unknown error occurred.", new Exception[] { ex, ex2 }));
+                        return processHandle = (null, new AggregateException(errUnableMsg + " Permissions were denied and an unknown error occurred.", new Exception[] { ex, ex2 }));
                     }
                 }
                 catch (Win32Exception ex) when (ex.NativeErrorCode is Win32ErrorCode.ERROR_INVALID_PARAMETER)
                 {
-                    return processHandle = (null, new ArgumentException(exMsg + " A process with ID " + ProcessId + " could not be found. The process may have exited.", ex));
+                    return processHandle = (null, new ArgumentException(errUnableMsg + " A process with ID " + ProcessId + " could not be found. The process may have exited.", ex));
                 }
                 catch (Exception ex)
                 {
                     // unknown error
-                    return processHandle = (null, new Exception(exMsg + " An unknown error occurred.", ex));
+                    return processHandle = (null, new Exception(errUnableMsg + " An unknown error occurred.", ex));
                 }
             }
             else
