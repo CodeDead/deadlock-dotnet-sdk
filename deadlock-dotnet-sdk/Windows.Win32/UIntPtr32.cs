5dd5b916c5de01bf1f0596d3f4915f1ecf8d9abf
Noah Sherwin <noahrsherwin@gmail.com>
2023-05-02 17:14:03 -0700

    refactor: move UIntPtr32<T> to new file

--- deadlock-dotnet-sdk/Windows.Win32/UIntPtr32.cs
+++ deadlock-dotnet-sdk/Windows.Win32/UIntPtr32.cs
@@ -12,14 +12,3 @@ public struct UIntPtr32
 
     public unsafe static explicit operator void*(UIntPtr32 v) => (void*)v.Value;
 }
-
-public struct UIntPtr32<T> where T : unmanaged
-{
-    public uint Value;
-
-    public static implicit operator UIntPtr32<T>(uint v) => new() { Value = v };
-    public static implicit operator uint(UIntPtr32<T> v) => v.Value;
-
-    public static explicit operator UIntPtr32(UIntPtr32<T> v) => v.Value;
-    public unsafe static explicit operator T*(UIntPtr32<T> v) => (T*)v.Value;
-}
