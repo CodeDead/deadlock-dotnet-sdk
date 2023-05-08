cbbc524587475f14f5561aaef804bbdc83e043d3
Noah Sherwin <noahrsherwin@gmail.com>
2023-05-07 20:39:06 -0700

    feat: add NameBuffer placeholder for trailing char array

--- deadlock-dotnet-sdk/Windows.Win32/System/WindowsProgramming/OBJECT_NAME_INFORMATION.cs
+++ deadlock-dotnet-sdk/Windows.Win32/System/WindowsProgramming/OBJECT_NAME_INFORMATION.cs
@@ -5,6 +5,7 @@ namespace Windows.Win32.System.WindowsProgramming;
 public struct OBJECT_NAME_INFORMATION
 {
     public UNICODE_STRING Name;
+    public unsafe fixed char NameBuffer[1];
 
     public string NameAsString => Name.ToStringLength();
 }
